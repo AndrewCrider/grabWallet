1f6ccb8f6409323acf337096302308529123d085
James Link <jamesmlink@gmail.com>
2017-02-22 22:39:06 -0500

    Version 0.1.0 live

--- content.js
+++ content.js
@@ -1,7 +1,5 @@
-window.onload = function () {
-	var newDiv = document.createElement("div");
-	newDiv.setAttribute("id", "gwdiv");
-	newDiv.innerHTML = '<p>Content to go here</p>';
-	document.body.insertBefore(newDiv, document.body.firstChild);
-	document.body.firstChild
-}
+var newDiv = document.createElement("div");
+newDiv.setAttribute("id", "gwdiv");
+newDiv.innerHTML = '<p><b>WARNING! <a href="https://grabyourwallet.org/">#grabyourwallet</a> says no.</b></p>';
+document.body.insertBefore(newDiv, document.body.firstChild);
+document.body.firstChild
